f8d8643eb3bd339a0d4a18285f60523970a4fb03
jsytal1 <jsytal1@gmail.com>
2023-12-17 11:19:56 -0500

    add education styles

--- app/ui/education-list.tsx
+++ app/ui/education-list.tsx
@@ -9,18 +9,23 @@ export default function Section() {
     <ul>
       {educations.map((education) => {
         return (
-          <li key={`${education.level}-${education.organization}`}>
-            <h3>
+          <li
+            className="mb-5 break-inside-avoid"
+            key={`${education.level}-${education.organization}`}
+          >
+            <h3 className="text-lg font-bold text-black">
               {education.level} - {education.area_of_study}
             </h3>
-            <i>{education.organization}</i>
-            <p>
+            <p className="text-md">
+              <i>{education.organization}</i>
+            </p>
+            <p className="text-gray-800 text-sm">
               <Time date={education.startDate} />
               <span> - </span>
               <Time date={education.endDate} />
             </p>
-            <ul>
-              <li>{education.gpa} GPA</li>
+            <ul className="ml-5 list-disc list-outside">
+              <li className="text-md">{education.gpa} GPA</li>
             </ul>
           </li>
         );
