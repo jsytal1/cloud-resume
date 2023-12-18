aff195c3ca07eab24fc8b0860532ef394a7ddc37
jsytal1 <jsytal1@gmail.com>
2023-12-17 20:05:45 -0500

    hide footer in print

--- app/ui/footer.tsx
+++ app/ui/footer.tsx
@@ -2,7 +2,7 @@ import VisitorCounter from "@/app/ui/visitor-counter";
 
 export default function Footer() {
   return (
-    <footer className="flex flex-row-reverse bg-gray-600 p-2">
+    <footer className="flex flex-row-reverse bg-gray-600 p-2 print:hidden">
       <VisitorCounter />
     </footer>
   );
