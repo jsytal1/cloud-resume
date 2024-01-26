4009450f28dcd651853176a398042306e38eac2b
jsytal1 <jsytal1@gmail.com>
2024-01-26 14:21:28 -0500

    update project list layout

--- app/ui/project-list.tsx
+++ app/ui/project-list.tsx
@@ -8,21 +8,21 @@ export default function Section() {
       {projects.map((item) => {
         return (
           <li className="mb-5 break-inside-avoid" key={item.name}>
-            <h3 className="text-xl font-bold text-black">{item.name}</h3>
-            <p className="text-lg">
-              <a
-                className="text-indigo-900"
-                href={`${item.url}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {item.url}
-              </a>
-            </p>
+            <h3 className="text-l font-bold text-black">{item.name}</h3>
             <ul className="ml-5 list-disc list-outside">
               {item.highlights.map((highlight, idx) => {
                 return <li key={idx}>{highlight}</li>;
               })}
+              <li>
+                <a
+                  className="text-indigo-900"
+                  href={`${item.url}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {item.url}
+                </a>
+              </li>
             </ul>
           </li>
         );
