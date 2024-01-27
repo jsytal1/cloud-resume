e97831e5c53ad68f47abd7b402fd4408a10ee7a8
jsytal1 <jsytal1@gmail.com>
2024-01-26 19:42:25 -0500

    update education date layouts

--- app/ui/education-list.tsx
+++ app/ui/education-list.tsx
@@ -13,16 +13,20 @@ export default function Section() {
             className="mb-5 break-inside-avoid"
             key={`${education.level}-${education.organization}`}
           >
-            <h3 className="text-lg font-bold text-black">
-              {education.level} - {education.area_of_study}
-            </h3>
+            <div className="flex justify-between items-baseline">
+              <div className="text-l font-bold text-black">
+                <h3 className="text-lg font-bold text-black">
+                  {education.level} - {education.area_of_study}
+                </h3>
+              </div>
+              <div className="text-l font-bold text-right">
+                <Time date={education.startDate} />
+                <span> - </span>
+                <Time date={education.endDate} />
+              </div>
+            </div>
             <p className="text-md">
               <i>{education.organization}</i>
-            </p>
-            <p className="text-gray-800 text-sm">
-              <Time date={education.startDate} />
-              <span> - </span>
-              <Time date={education.endDate} />
             </p>
             <ul className="ml-5 list-disc list-outside">
               <li className="text-md">{education.gpa} GPA</li>
