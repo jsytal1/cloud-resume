6417b5f8c861acdc53c574b222bc238eae74488d
jsytal1 <jsytal1@gmail.com>
2024-01-29 17:59:12 -0500

    project-list: add date, more compact layout

--- app/ui/project-list.tsx
+++ app/ui/project-list.tsx
@@ -1,5 +1,7 @@
 import { fetchProjects } from "@/app/lib/data";
 
+import Time from "@/app/ui/time";
+
 export default function Section() {
   const projects = fetchProjects();
 
@@ -7,22 +9,32 @@ export default function Section() {
     <ul>
       {projects.map((item) => {
         return (
-          <li className="mb-5 break-inside-avoid" key={item.name}>
-            <h3 className="text-l font-bold text-black">{item.name}</h3>
+          <li className="mb-5 print:mb-3 break-inside-avoid" key={item.name}>
+            <div className="flex justify-between items-baseline w-full">
+              <h3 className="text-l font-bold text-black">{item.name}</h3>
+              <p className="text-base text-right font-bold">
+                <Time date={item.date}></Time>
+              </p>
+            </div>
             <ul className="ml-5 list-disc list-outside">
               {item.highlights.map((highlight, idx) => {
-                return <li key={idx}>{highlight}</li>;
+                return (
+                  <li key={idx}>
+                    {highlight}
+                    {idx === 0 && item.url && " - "}
+                    {idx === 0 && item.url && (
+                      <a
+                        className="text-indigo-900"
+                        href={`${item.url}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {item.url}
+                      </a>
+                    )}
+                  </li>
+                );
               })}
-              <li>
-                <a
-                  className="text-indigo-900"
-                  href={`${item.url}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {item.url}
-                </a>
-              </li>
             </ul>
           </li>
         );
