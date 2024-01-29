2d054fa3fad05f59addd21d35c6c63dea8828d1b
jsytal1 <jsytal1@gmail.com>
2024-01-29 17:57:45 -0500

    smaller seciton margins in print

--- app/ui/section.tsx
+++ app/ui/section.tsx
@@ -6,7 +6,7 @@ export default function Section({
   name: string;
 }) {
   return (
-    <section className="mt-3">
+    <section className="mt-3 print:mt-1">
       <div className="flex items-baseline justify-center">
         <h2 className="mb-1 m-0 text-xl font-bold text-black">{name} </h2>
         <div className="ml-2 pb-1.5 flex-grow border-t-2 border-black"></div>
