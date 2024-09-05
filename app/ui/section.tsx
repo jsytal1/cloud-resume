b2fb91c6afdfe2a5d14aef5b52d2bcb3f9745e3a
jsytal1 <jsytal1@gmail.com>
2024-09-05 16:14:38 -0400

    refactor: Update section heading style to avoid line breaks

--- app/ui/section.tsx
+++ app/ui/section.tsx
@@ -8,7 +8,7 @@ export default function Section({
   return (
     <section className="mt-0 print:mt-1">
       <div className="flex items-baseline justify-center">
-        <h2 className="mb-1 text-xl font-bold text-black">{name} </h2>
+        <h2 className="mb-1 text-xl font-bold text-black break-after-avoid">{name} </h2>
         <div className="ml-2 pb-1.5 flex-grow border-t-2 border-black"></div>
       </div>
       {children}
